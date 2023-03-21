3e6c75ba54c6265a979043fc1994cc217a93fa19
david <leachda12@icloud.com>
2023-03-21 10:34:26 -0400

    added Google Maps API

--- maps/src/Company.ts
+++ maps/src/Company.ts
@@ -1,7 +1,7 @@
 import { faker } from '@faker-js/faker';
 
 export class Company {
-    companyName: string;
+    name: string;
     catchPhrase: string;
     location: {
         lat: number;
@@ -9,7 +9,7 @@ export class Company {
     };
 
     constructor() {
-        this.companyName = faker.company.name();
+        this.name = faker.company.name();
         this.catchPhrase = faker.company.catchPhrase();
         this.location = {
             lat: parseFloat(faker.address.latitude()),
