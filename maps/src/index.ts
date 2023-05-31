71c966e4491547abf36b129e2e938c3920660cfc
mononokai <leachda12@icloud.com>
2023-05-31 00:37:09 -0400

    Added API key to .env file

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,9 +1,21 @@
 import { User } from './User';
 import { Company } from './Company';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+const GOOGLE_MAPS_API_KEY = process.env.MAP_KEY;
+
+const script = document.createElement('script');
+script.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}`;
+script.defer = true;
+
+document.head.appendChild(script);
 
 const user = new User;
 const company = new Company
 
 console.log(user)
 console.log(company)
+
+google
