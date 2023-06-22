ac3c4ba0d16fb34a16fe5b80dee87baa78b20066
mononokai <leachd12@icloud.com>
2023-06-22 00:22:46 -0400

    feat: add marker popups

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -22,13 +22,21 @@ export class CustomMap {
         });
     }
 
-    addUserMarker(mappable: Mappable): void {
-        new google.maps.Marker({
+    addMarker(mappable: Mappable): void {
+        const marker = new google.maps.Marker({
             map: this.googleMap,
             position: {
                 lat: mappable.location.lat,
                 lng: mappable.location.lng
             }
+        });
+
+        marker.addListener('click', () => {
+            const infoWindow = new google.maps.InfoWindow({
+                content: 'Hi there!'
+            });
+            
+            infoWindow.open(this.googleMap, marker);
         });
     }
     
--- maps/src/index.ts
+++ maps/src/index.ts
@@ -15,11 +15,10 @@ const script = document.createElement('script');
 script.onload = function () {
     const user = new User;
     const company = new Company;
-    
     const customMap = new CustomMap('map');
 
-    customMap.addUserMarker(user);
-    customMap.addCompanyMarker(company);
+    customMap.addMarker(user);
+    customMap.addMarker(company);
 }
 
 
