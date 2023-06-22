9d3aa4a46bc66fdd8288f37143eb80c12fca4263
mononokai <leachd12@icloud.com>
2023-06-21 23:49:01 -0400

    feat: add interface for addMarker args

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,5 +1,14 @@
 import { User } from './User';
 import { Company } from './Company';
+
+// Interface for addMarker args
+interface Mappable {
+    location: {
+        lat: number;
+        lng: number;
+    }
+}
+
 export class CustomMap {
     googleMap: google.maps.Map;
 
@@ -13,23 +22,25 @@ export class CustomMap {
         });
     }
 
-    addUserMarker(user: User): void {
+    addUserMarker(mappable: Mappable): void {
         new google.maps.Marker({
             map: this.googleMap,
             position: {
-                lat: user.location.lat,
-                lng: user.location.lng
+                lat: mappable.location.lat,
+                lng: mappable.location.lng
             }
         });
     }
     
-    addCompanyMarker(company: Company): void {
-        new google.maps.Marker({
-            map: this.googleMap,
-            position: {
-                lat: company.location.lat,
-                lng: company.location.lng
-            }
-        });
-    }
+//     addCompanyMarker(company: Company): void {
+//         new google.maps.Marker({
+//             map: this.googleMap,
+//             position: {
+//                 lat: company.location.lat,
+//                 lng: company.location.lng
+//             }
+//         });
+//     }
+
+
 }
