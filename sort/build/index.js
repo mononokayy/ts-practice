34c5c1f485865170ccdbf061241c13ad5e848d2c
mononokai <leachd12@icloud.com>
2023-06-22 21:49:53 -0400

    feat: add Sorter class

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,6 +1,32 @@
 "use strict";
-console.log('hi');
-const logThis = () => {
-    console.log('this');
-};
-logThis();
+class Sorter {
+    // using the public modifier allows us to remove these extra lines
+    // collection: number[];
+    constructor(collection) {
+        this.collection = collection;
+        //     this.collection = collection;
+    }
+    sort() {
+        const { length } = this.collection;
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - i - 1; j++) {
+                // comparing two values from the array
+                // this normally only works if collection is an array of numbers
+                // adding the "instanceof Array" creates a type guard that allows us to use Array properties rather than just properties shared between a number array and a string
+                if (this.collection instanceof Array) {
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        const leftSide = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = leftSide;
+                    }
+                }
+                // this creates another type guard searching for a string
+                if (typeof this.collection === 'string') {
+                }
+            }
+        }
+    }
+}
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,37 @@
-console.log('hi');
+class Sorter {
+    // using the public modifier allows us to remove these extra lines
+    // collection: number[];
 
-const logThis = () => {
-    console.log('this');
-};
+    constructor(public collection: number[] | string) {
+    //     this.collection = collection;
+    }
 
-logThis();
+    sort(): void {
+        const { length } = this.collection;
+
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - i - 1; j++) {
+                
+                // comparing two values from the array
+                // this normally only works if collection is an array of numbers
+                // adding the "instanceof Array" creates a type guard that allows us to use Array properties rather than just properties shared between a number array and a string
+                if (this.collection instanceof Array) {
+                        if (this.collection[j] > this.collection[j + 1]) {
+                        const leftSide = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j+1] = leftSide
+                    }
+                }
+
+                // this creates another type guard searching for a string
+                if (typeof this.collection === 'string') {
+
+                }
+            }
+        }
+    }
+}
+
+const sorter = new Sorter([10, 3, -5, 0])
+sorter.sort();
+console.log(sorter.collection)
