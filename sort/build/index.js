3833d4eabb3799fbbe76571a7274380abdc75af2
mononokai <leachda12@icloud.com>
2023-06-28 18:58:38 -0400

    built CharacterCollection class

--- sort/build/index.js
+++ sort/build/index.js
@@ -2,7 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const NumbersCollection_1 = require("./NumbersCollection");
 const Sorter_1 = require("./Sorter");
-const numbersCollection = new NumbersCollection_1.NumbersCollection([10, 30, -5, 0]);
-const sorter = new Sorter_1.Sorter(numbersCollection);
+// const numbersCollection = new NumbersCollection([10, 30, -5, 0]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+// const charactersCollection = new CharactersCollection('Xaayb');
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection.data);
 sorter.sort();
 console.log(numbersCollection.data);
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,5 +1,16 @@
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 import { Sorter } from './Sorter';
+// const numbersCollection = new NumbersCollection([10, 30, -5, 0]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+
+
+// const charactersCollection = new CharactersCollection('Xaayb');
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection.data);
 
 const numbersCollection = new NumbersCollection([10, 30, -5, 0]);
 const sorter = new Sorter(numbersCollection);
