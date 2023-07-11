c4d28de5b43a7a17da568ed41348a7a2887929aa
mononokai <leachda12@icloud.com>
2023-07-11 07:08:21 -0400

    feat: add login and logout

--- server/src/routes/loginRoutes.ts
+++ server/src/routes/loginRoutes.ts
@@ -1,10 +1,24 @@
-import { Router, Request, Response } from "express";
+import { Router, Request, Response, NextFunction } from "express";
+
+interface RequestWithBody extends Request {
+    body: { [key: string]: string | undefined }
+}
+
+function requireAuth(req: Request, res: Response, next: NextFunction): void {
+    if (req.session && req.session.loggedIn) {
+        next();
+        return;
+    }
+
+    res.status(403);
+    res.send('Not permitted');
+}
 
 const router = Router();
 
 router.get('/login', (req: Request, res: Response) => {
     res.send(`
-        <form method="post">
+        <form method="POST">
             <div>
                 <label>Email</label>
                 <input name="email" />
@@ -18,10 +32,46 @@ router.get('/login', (req: Request, res: Response) => {
     `);
 });
 
-router.post('/login', (req: Request, res: Response) => {
+router.post('/login', (req: RequestWithBody, res: Response) => {
     const { email, password } = req.body;
 
-    res.send(email + password);
+    if (email && password && email === "test@test.com" && password === 'test') {
+        // mark this person as logged in
+        req.session = { loggedIn: true };
+        // redirect them to the root route
+        res.redirect('/');
+    }
+    else {
+        res.send('Invalid email or password');
+    }
+});
+
+router.get('/', (req: Request, res: Response) => {
+    if (req.session && req.session.loggedIn) {
+        res.send(`
+        <div>
+            <div>You are logged in</div>
+            <a href="/logout">Logout</a>
+        </div>
+        `)
+    }
+    else {
+        res.send(`
+        <div>
+            <div>You are not logged in</div>
+            <a href="/login">Login</a>
+        </div>
+        `)
+    }
+});
+
+router.get('/logout', (req: Request, res: Response) => {
+    req.session = undefined;
+    res.redirect('/');
+});
+
+router.get('/protected', requireAuth, (req: Request, res: Response) => {
+    res.send('Welcome to protected route, logged in user');
 });
 
 export { router };
